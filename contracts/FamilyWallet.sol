f3928b3196a1a75dd36fa5f39bc47e2e17a0537e
Deon <deonsummers91@gmail.com>
2018-03-06 12:07:25 -0500

    remove back package.json from .gitignore

---
+++ contracts/FamilyWallet.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.18;
+
+contract FamilyWallet {
+  function FamilyWallet() {
+
+  }
+
+  function assignFamily(address[] members) {
+
+  }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var FamilyWallet = artifacts.require("./FamilyWallet.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(FamilyWallet);
 };
