d7b9548b64a50be102c292fdcbc8b117c2a82969
Deon <deonsummers91@gmail.com>
2018-03-06 18:06:02 -0500

    add more goal tracking shenanigans

--- contracts/GoalTracker.sol
+++ contracts/GoalTracker.sol
@@ -1,17 +1,21 @@
 // Tracks each persons goals and send rewards
+import "./FamilyWallet.sol";
+
+
 pragma solidity ^0.4.18;
 
 contract GoalTracker {
   mapping(address => bytes32[]) public goalDescriptions; // hopefully bytes32 fits
   mapping(address => bool[]) public goalCompleted;
-  uint8 public threshold; // number of goals before funds are dispensed (maybe should be percentage? idk) 
-  uint8 public award; // amount of funds in wei sent to user (or family) when rewards are dispensed
+  uint256 public threshold; // number of goals before funds are dispensed (maybe should be percentage? idk) 
+  uint256 public award; // amount of funds in wei sent to user (or family) when rewards are dispensed
 
-  function GoalTracker() public {
-
+  function GoalTracker(uint256 _threshold, uint256 _award) public {
+    threshold = _threshold;
+    award = _award;
   }
   
-  function addGoals(address _member, bytes32 _description) public {
+  function addGoals(address _member, bytes32[] _description) public {
     for (uint256 i = 0; i < _description.length; i++) {
       goalDescriptions[_member].push(_description[i]);
     }
@@ -31,6 +35,27 @@ contract GoalTracker {
 
   // @note this is gas intensive af
   function completeGoal(address _member, bytes32 _description) public {
-    
+    bytes32[] storage descriptions = goalDescriptions[_member];
+    for (uint256 i = 0; i < descriptions.length; i++) {
+      if (descriptions[i] == _description) {
+        goalCompleted[_member][i] = true;
+        if(checkGoals(goalCompleted[_member])) {
+         _member.transfer(award); 
+        }
+        break;
+      }
+    }
+  }
+
+  function checkGoals(bool[] goals) internal returns (bool) {
+    uint256 count = 0;
+    for (uint256 i = 0; i < goals.length; i++) {
+      if(goals[i] == true) {
+        count++;
+        if (threshold == count) {
+          return true;
+        }
+      }
+    }
   }
 }
--- test/goaltracker.js
+++ test/goaltracker.js
@@ -10,8 +10,14 @@ contract('goalTracker', function(accounts) {
   it("should be able to create a goal", async function() {
     await goalTracker.addGoals(accounts[0], ["test goal one", "test goal two"]);
     let goals = await goalTracker.goals(accounts[0]);
-    assert.equal(goals[0][0], "test goal one");
-    assert.equal(goal[0][1], false);
+    assert.equal(web3.toUtf8(goals[0][0]), "test goal one");
+    assert.equal(goals[1][0], false);
   });
 
+  it("should be able to complete a task", async function() {
+    await goalTracker.completeGoal(accounts[0], "test goal one");
+    let goals = await goalTracker.goals(accounts[0]);
+    assert.equal(web3.toUtf8(goals[0][0]), "test goal one");
+    assert.equal(goals[1][0], true);
+  });
 });
